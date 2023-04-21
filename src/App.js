ceb7f403d0e2d31a64c71993b684e759693e36f6
Tim Wang <weihewang2012@gmail.com>
2023-04-21 10:40:00 -0700

    Chore: run formatter on JS files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { React, useEffect, useState } from 'react';
-import axios from 'axios';
-import { Tweet, NewTweet } from './tweet';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+import { Tweet, NewTweet } from "./tweet";
 
 // The Tweets component displays tweets for a user
 function Tweets({ user, reloadTweets, setReloadTweets }) {
@@ -13,64 +13,74 @@ function Tweets({ user, reloadTweets, setReloadTweets }) {
   const [tweets, setTweets] = useState([]);
 
   const deleteTweet = async (tweetId) => {
-    axios.delete(`/tweets/${tweetId}?user=${user.name}`)
-      .then(
-        (response) => {
-          console.log(response);
-          setReloadTweets(!reloadTweets);
-        }
-      ).catch(
-        console.error
-      );
-  }
+    axios
+      .delete(`/tweets/${tweetId}?user=${user.name}`)
+      .then((response) => {
+        console.log(response);
+        setReloadTweets(!reloadTweets);
+      })
+      .catch(console.error);
+  };
 
   useEffect(() => {
-    const fetchTweets = async () => axios.get(
-      `/tweets/${user.name}`
-    ).then(
-      (response) => {
-        setTweets(response.data.tweets)
-        setLoading(false);
-      }
-    ).catch(
-      err => console.log(err)
-    );
+    const fetchTweets = async () =>
+      axios
+        .get(`/tweets/${user.name}`)
+        .then((response) => {
+          setTweets(response.data.tweets);
+          setLoading(false);
+        })
+        .catch((err) => console.log(err));
 
     fetchTweets();
-  }, [reloadTweets, user])
+  }, [reloadTweets, user]);
 
   if (loading) {
-    return <h2>Loading tweets</h2>
+    return <h2>Loading tweets</h2>;
   }
   if (tweets) {
     return (
       <Container>
         <h2>Tweets from {user.name}</h2>
-        <Row className='justify-content-center'>
-          <Col className='col-auto'>
-            {tweets.map(tweet => <Tweet tweet={tweet} deleteCallback={() => deleteTweet(tweet.id)} key={tweet.id} />)}
+        <Row className="justify-content-center">
+          <Col className="col-auto">
+            {tweets.map((tweet) => (
+              <Tweet
+                tweet={tweet}
+                deleteCallback={() => deleteTweet(tweet.id)}
+                key={tweet.id}
+              />
+            ))}
           </Col>
-
         </Row>
       </Container>
-    )
+    );
   }
-  return (<h2>User's tweets goes here</h2>)
+  return <h2>User's tweets goes here</h2>;
 }
 
 function App() {
-  const [user, setUser] = useState({ name: 'tim' })
+  const [user, setUser] = useState({ name: "tim" });
   const [reloadTweets, setReloadTweets] = useState(false);
   return (
     <>
-      <div className='App'>
+      <div className="App">
         <h1>Mini Twitter</h1>
-        <NewTweet user={user} setUser={setUser} reloadTweets={reloadTweets} setReloadTweets={setReloadTweets} />
+        <NewTweet
+          user={user}
+          setUser={setUser}
+          reloadTweets={reloadTweets}
+          setReloadTweets={setReloadTweets}
+        />
         <br></br>
         <br></br>
 
         <Container>
-          <Tweets user={user} reloadTweets={reloadTweets} setReloadTweets={setReloadTweets} />
+          <Tweets
+            user={user}
+            reloadTweets={reloadTweets}
+            setReloadTweets={setReloadTweets}
+          />
         </Container>
       </div>
     </>
--- src/tweet.js
+++ src/tweet.js
@@ -1,104 +1,117 @@
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { useEffect, useRef, useState } from 'react';
-import axios from 'axios';
-
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Dropdown from "react-bootstrap/Dropdown";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
 export function Tweet({ tweet, deleteCallback }) {
-    return (
-        <Card>
-            <Card.Body>
-                <Card.Header style={{ color: 'blue' }}>{tweet.created_at}</Card.Header>
-                <Card.Text>{tweet.text}</Card.Text>
-                <Button className='btn-warning' onClick={deleteCallback}>Delete</Button>
-            </Card.Body>
-        </Card>
-    )
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Header style={{ color: "blue" }}>{tweet.created_at}</Card.Header>
+        <Card.Text>{tweet.text}</Card.Text>
+        <Button className="btn-warning" onClick={deleteCallback}>
+          Delete
+        </Button>
+      </Card.Body>
+    </Card>
+  );
 }
 
 function UserDropdown({ setUser, setCurrentUser }) {
-    const [usernames, setUsernames] = useState([]);
+  const [usernames, setUsernames] = useState([]);
 
-    const handleSelect = (k, _e) => {
-        setUser({ name: k });
-        setCurrentUser(k);
-    }
+  const handleSelect = (k, _e) => {
+    setUser({ name: k });
+    setCurrentUser(k);
+  };
 
-    useEffect(() => {
-        const fetchUsers = async () => axios.get('/users')
-            .then((response) => {
-                setUsernames(response.data.users);
-            })
-            .catch(console.error);
+  useEffect(() => {
+    const fetchUsers = async () =>
+      axios
+        .get("/users")
+        .then((response) => {
+          setUsernames(response.data.users);
+        })
+        .catch(console.error);
 
-        fetchUsers();
-    }, []);
+    fetchUsers();
+  }, []);
 
-    return (
-        <Dropdown onSelect={handleSelect}>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Select User
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-                {
-                    usernames.map(username => <Dropdown.Item key={username} eventKey={username}>{username}</Dropdown.Item>)
-                }
-            </Dropdown.Menu>
-        </Dropdown>
-    )
+  return (
+    <Dropdown onSelect={handleSelect}>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Select User
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        {usernames.map((username) => (
+          <Dropdown.Item key={username} eventKey={username}>
+            {username}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
 }
 
 // The New Tweet component defines a form for posting new tweets
 export function NewTweet({ user, setUser, reloadTweets, setReloadTweets }) {
-    const newTweetContentRef = useRef();
-    const newTweetFormRef = useRef();
-    const [currentUser, setCurrentUser] = useState('guest')
-    const handlePost = async (e) => {
-        e.preventDefault();
-        const data = {
-            user: user.name,
-            text: newTweetContentRef.current.value,
-        };
-        axios.post(
-            '/tweets',
-            JSON.stringify(data),
-        ).then(
-            setReloadTweets(!reloadTweets)
-        ).then(
-            // clears input after posting
-            () => newTweetFormRef.current.reset()
-        ).catch(
-            err => console.error(err)
-        );
-    }
+  const newTweetContentRef = useRef();
+  const newTweetFormRef = useRef();
+  const [currentUser, setCurrentUser] = useState("guest");
+  const handlePost = async (e) => {
+    e.preventDefault();
+    const data = {
+      user: user.name,
+      text: newTweetContentRef.current.value,
+    };
+    axios
+      .post("/tweets", JSON.stringify(data))
+      .then(setReloadTweets(!reloadTweets))
+      .then(
+        // clears input after posting
+        () => newTweetFormRef.current.reset()
+      )
+      .catch((err) => console.error(err));
+  };
 
-    return (
-        <div>
-            <Form ref={newTweetFormRef}>
-                <Container>
-                    <Form.Group>
-                        <Row className='justify-content-center'>
-                            <Col className='col-auto'>
-                                <UserDropdown setUser={setUser} setCurrentUser={setCurrentUser}></UserDropdown>
-                            </Col>
-                            <Col className='col-sm-1'>
-                                <p><b>{currentUser}</b></p>
-                            </Col>
-                            <Col className='col-md-6'>
-                                <Form.Control ref={newTweetContentRef} placeholder='new tweet' id='tweet-text-input'>
-                                </Form.Control>
-                            </Col>
-                            <Col className='col-auto'><Button type='submit' onClick={handlePost}>Post</Button>
-                            </Col>
-                        </Row>
-                    </Form.Group>
-                </Container>
-            </Form>
-        </div>
-    );
+  return (
+    <div>
+      <Form ref={newTweetFormRef}>
+        <Container>
+          <Form.Group>
+            <Row className="justify-content-center">
+              <Col className="col-auto">
+                <UserDropdown
+                  setUser={setUser}
+                  setCurrentUser={setCurrentUser}
+                ></UserDropdown>
+              </Col>
+              <Col className="col-sm-1">
+                <p>
+                  <b>{currentUser}</b>
+                </p>
+              </Col>
+              <Col className="col-md-6">
+                <Form.Control
+                  ref={newTweetContentRef}
+                  placeholder="new tweet"
+                  id="tweet-text-input"
+                ></Form.Control>
+              </Col>
+              <Col className="col-auto">
+                <Button type="submit" onClick={handlePost}>
+                  Post
+                </Button>
+              </Col>
+            </Row>
+          </Form.Group>
+        </Container>
+      </Form>
+    </div>
+  );
 }
